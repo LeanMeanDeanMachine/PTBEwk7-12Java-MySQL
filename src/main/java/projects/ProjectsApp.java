6e00a2945c65e9ae5803a013bf2cf7dedb8088b3
LeanMeanDeanMachine <126035068+LeanMeanDeanMachine@users.noreply.github.com>
2023-05-13 16:56:26 -0600

    Deleted DbConnection.getConnection(); method from main

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -4,12 +4,6 @@ import java.sql.Connection;
 import projects.dao.DbConnection;
 public class ProjectsApp {
     public static void main(String[] args) {
-        try {
-            Connection conn = DbConnection.getConnection();
-            System.out.println("Connection Successful!");
-        } catch (Exception e) {
-            System.out.println("Error connecting to database.");
-            e.printStackTrace();
-        }
+
     }
 }
